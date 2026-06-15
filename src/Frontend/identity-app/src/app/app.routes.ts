3ec1886e3bfe4fe73dc7eaca8c4ccfcd91bba72f
Okunev <ilyaokubev@gmail.com>
2026-06-15 23:11:52 +0400

    refactor: Cleanup old components

--- src/Frontend/identity-app/src/app/app.routes.ts
+++ src/Frontend/identity-app/src/app/app.routes.ts
@@ -1,15 +1,8 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './components/login/login';
 import { CallbackComponent } from './components/callback/callback';
-import { RegisterComponent } from './components/register/register';
-import { ProfileComponent } from './components/profile/profile';
-import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
   { path: 'callback', component: CallbackComponent },
-  { path: 'profile', component: ProfileComponent, canActivate: [authGuard] },
-  { path: '', redirectTo: '/profile', pathMatch: 'full' },
-  { path: '**', redirectTo: '/login' }
+  { path: '', pathMatch: 'full', redirectTo: 'callback' },
+  { path: '**', redirectTo: 'callback' },
 ];
